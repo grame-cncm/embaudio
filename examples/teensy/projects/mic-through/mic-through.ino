ae649360e8b83f00cbd34680f771704dbb916126
Romain Michon <rmnmichon@gmail.com>
2022-09-23 16:14:27 +0200

    Adding mic through example

---
+++ examples/teensy/projects/mic-through/mic-through.ino
@@ -0,0 +1,20 @@
+#include <Audio.h>
+
+AudioInputI2S in;
+AudioOutputI2S out;
+AudioControlSGTL5000 audioShield;
+AudioConnection patchCord0(in,0,out,0);
+AudioConnection patchCord1(in,0,out,1);
+
+void setup() {
+  Serial.begin(9600);
+  AudioMemory(6);
+  audioShield.enable();
+  audioShield.inputSelect(AUDIO_INPUT_MIC);
+  audioShield.inputLevel(1);
+  audioShield.micGain(10); // in dB
+  audioShield.volume(1);
+}
+
+void loop() {
+}
